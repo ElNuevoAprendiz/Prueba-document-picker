4231ac416b0697ff6025401968a3cc4b682c9f1f
Dany <profedanieljuan@gmail.com>
2025-12-06 16:18:46 -0300

    Se enlaza a git local con github

--- App.tsx
+++ App.tsx
@@ -1,24 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 import DocumentPicker from 'react-native-document-picker';
 import React, { useState } from 'react';
 import { Button, Text, View } from 'react-native';
